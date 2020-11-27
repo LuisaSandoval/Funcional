6bae220de810e1371695205b3d9ed50a3d71d094
LuisaSandoval <72033699+LuisaSandoval@users.noreply.github.com>
2020-11-27 11:05:56 -0700

    Add files via upload

---
+++ Introduccion/funcionespredefinidas.hs
@@ -0,0 +1,30 @@
+
+fun1 x = abs x
+
+fun2 x = ceiling x
+
+fun3 x = succ x
+
+fun4 x y = min x y 
+
+fun5 x y = max x y
+
+fun6 x y = div x y
+
+fun7 x y = mod x y
+
+fun8 x = even x
+
+fun9 x = floor x
+
+fun10 x y = gcd x y
+
+fun11 x = not x
+
+fun12 x = odd x
+
+fun13 x = succ x
+
+fun14 x = signum (-x)
+
+fun15 x y = snd(x,y)
