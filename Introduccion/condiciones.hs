cfe17fe1202cfba2e3bb53065075db0790a11c79
LuisaSandoval <72033699+LuisaSandoval@users.noreply.github.com>
2020-12-10 10:33:28 -0700

    Add files via upload

---
+++ Introduccion/condiciones.hs
@@ -0,0 +1,25 @@
+sumarValor x= if x >20 then x+10 else x
+
+opciones x  | (x == 0) = 0
+            | (x == 1) = 1
+            | otherwise = 10
+
+suma :: Int -> Int -> Int
+suma a b = a + b
+
+opand :: Bool -> Bool -> Bool
+opand True True = True
+opand False False = False
+
+cadena :: String -> String
+cadena x = x
+
+miArreglin :: (Int, Int) -> (Int, Int) -> (Int, Int)
+miArreglin (a,b) (c,d) = (a+c, b+d)
+
+persona :: (String, Int, String)
+persona = ("juan",12,"Admin")
+
+lista (x:xs) = xs
+lista2 x = head x
+
